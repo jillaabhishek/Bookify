ff8b4e4f7d2a18042476f2a959c427a6edea2d8a
ABHISHEK\jilla <jilla.abhishek94@gmail.com>
2023-09-30 17:21:58 +0530

    Implemeted the Domain Layer of Apartment Booking Site

---
+++ Bookify.Domain/Shared/Currency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Shared
+{
+    public record Currency
+    {
+        internal static readonly Currency None = new("");
+        public static readonly Currency Usd = new("USD");
+        public static readonly Currency Eur = new("EUR");
+
+        private Currency(string code) => Code = code;
+
+        public string Code { get; init; }
+
+        public static Currency FromCode(string code)
+        {
+            return All.FirstOrDefault(x => x.Code == code) ??
+                throw new ApplicationException("The Currency code is invalid");
+        }
+
+        public static readonly IReadOnlyCollection<Currency> All = new[] { Usd, Eur };
+    }
+}
